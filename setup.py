b387390dd2d13ba52195fc93a0e682d4266757ec
Susan Tyler <susanltyler@gmail.com>
2019-02-14 22:49:18 -0500

    Added python version classifiers in setup.py

--- setup.py
+++ setup.py
@@ -80,6 +80,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     description='Convert HTML to ANS',
     long_description=LONG_DESCRIPTION,
