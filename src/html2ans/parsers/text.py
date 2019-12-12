0e8b7fa5ceff9d5fe1d6d75eb85a9ef4a6b49835
Jackson Toomey <jacksontoomey@gmail.com>
2019-12-12 10:22:35 -0500

    fixes error when embeds were improperly formatted and contained &nbsp;

--- src/html2ans/parsers/text.py
+++ src/html2ans/parsers/text.py
@@ -185,9 +185,12 @@ class BlockquoteParser(AbstractTextParser):
                             content_elements.append(p_result.output)
             else:
                 p_result = ParagraphParser().parse(element)
+                if p_result.match and p_result.output:
+                    content_elements.append(p_result.output)
                 if p_result.match:
                     content_elements.append(p_result.output)
-        if match:
+        content_elements = [el for el in content_elements if el is not None]
+        if match and content_elements:
             result = {
                 "type": "quote",
                 "content_elements": content_elements
